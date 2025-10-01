26eddda544ca4509e893cdd9f0cb76d8e5a590ba
kormonditamas2222 <t.kormond2006@gmail.com>
2025-10-01 08:33:46 +0200

    classes

---
+++ kosar2004/Csapat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosar2004
+{
+    class Csapat
+    {
+        string name;
+        int pontszam;
+
+        public Csapat(string name, int pontszam)
+        {
+            this.name = name;
+            this.pontszam = pontszam;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Pontszam { get => pontszam; set => pontszam = value; }
+        public override string? ToString()
+        {
+            return $"{name} {pontszam}";
+        }
+    }
+}
---
+++ kosar2004/Merkozes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosar2004
+{
+    class Merkozes
+    {
+        Csapat hazai;
+        Csapat vendeg;
+        string helyszin;
+        DateTime idopont;
+
+        public Merkozes(Csapat hazai, Csapat vendeg, string helyszin, DateTime idopont)
+        {
+            this.hazai = hazai;
+            this.vendeg = vendeg;
+            this.helyszin = helyszin;
+            this.idopont = idopont;
+        }
+
+        public string Helyszin { get => helyszin; set => helyszin = value; }
+        public DateTime Idopont { get => idopont; set => idopont = value; }
+        internal Csapat Hazai { get => hazai; set => hazai = value; }
+        internal Csapat Vendeg { get => vendeg; set => vendeg = value; }
+
+        public override string? ToString()
+        {
+            return $"{hazai} {vendeg} {helyszin} {idopont}";
+        }
+    }
+}
