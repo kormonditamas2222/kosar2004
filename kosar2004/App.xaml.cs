64adcfd4cf24c73ed16d64634a7ec6cbe8c8e193
kormonditamas2222 <t.kormond2006@gmail.com>
2025-10-01 08:25:40 +0200

    Add project files.

---
+++ kosar2004/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace kosar2004
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
